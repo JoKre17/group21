8d3717cc2e95cd1ec88e747eb2b644ea8bb7d253
Wolfgang Gritz <wolfgang.gritz@hotmail.de>
2016-12-23 17:47:23 +0100

    initial design - feel free to delete/modify anything, just a glympse

--- JavaScript/client.js
+++ JavaScript/client.js
@@ -1,3 +1,46 @@
 /**
  * 
  */
+ 
+var vehicle = {
+	0 : {
+		name : "Lamborghini"
+	},
+	1 : {
+		name : "Ferrari"
+	}
+} 
+
+window.onload = function(){
+	init();
+}
+
+function $(elem){
+	return document.getElementById(elem);
+}
+
+function init(){
+	console.log("window loaded");
+	initLayout();
+}
+
+function initLayout(){
+	$("drunk-container").onclick = function(){
+		console.log("clicked drunk-container");
+		$("drunk_menu-container").style.display = "block";
+		$("drunk_menu-container").appendChild(createTextField(vehicle));
+	}
+}
+
+function createTextField(type){
+	var textfield = document.createElement("INPUT");
+	textfield.type = "TEXT";
+	textfield.placeholder = "please type...";
+	textfield.onkeyup = function(){
+		for (var key in type){
+			console.log(type[key].name);
+		}
+	}
+	
+	return textfield;
+}
