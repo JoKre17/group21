75ac84634fae88cd4dd9a6f2ed8b5525433a8e48
JoKre17 <josef.kriegel@web.de>
2017-01-08 18:10:09 +0100

    Drunk Mode Menu

--- JavaScript/client.js
+++ JavaScript/client.js
@@ -53,6 +53,17 @@ function openPage(pageId){
 		width:"100%",
 		height:"100%"
 	}, 800);
+	
+	switch($(pageId).attr('id')){
+		case 'drunk_menu-container':
+			console.log($(pageId).attr('id') + " openend.");
+			var drunk_menu_container = document.getElementById($(pageId).attr('id'));
+			clearPage(pageId);
+			buildLocationDetectionScreen(drunk_menu_container);
+			window.setTimeout(showDrunkMenuDialog, 1000, drunk_menu_container);
+			break;
+			
+	}
 }
 
 function closePage(pageId){
@@ -61,6 +72,10 @@ function closePage(pageId){
 		height:"0"
 	}, 800);
 	$(pageId).fadeOut(0);
+}
+
+function clearPage(pageId){
+	$(pageId).empty();
 }
 
 
@@ -76,3 +91,74 @@ function createTextField(type){
 	
 	return textfield;
 }
+
+function buildLocationDetectionScreen(parentDiv){
+	var locationScreen = document.createElement('div');
+	
+	var loadingIconImage = new Image();
+	loadingIconImage.src = 'https://media.giphy.com/media/l0HlRF0GurmvjniFO/giphy.gif';
+	loadingIconImage.style.display = '-webkit-box';
+	loadingIconImage.style.paddingTop = '50%';
+	loadingIconImage.style.paddingLeft = '20%';
+	loadingIconImage.style.width = '60%';
+	
+	var infoLabel = document.createElement('label');
+	infoLabel.innerHTML = "You are being located";
+	infoLabel.style.display = '-webkit-box';
+	infoLabel.style.textAlign = '-webkit-center';
+	infoLabel.style.paddingTop = '10%';
+	
+	
+	parentDiv.appendChild(loadingIconImage);
+	parentDiv.appendChild(infoLabel);
+}
+
+function showDrunkMenuDialog(drunk_menu_container) {
+	
+	clearPage("#" + drunk_menu_container.id);
+	
+	var infoLabel = document.createElement('label');
+	infoLabel.innerHTML = "Select your destination";
+	infoLabel.style.display = '-webkit-box';
+	infoLabel.style.textAlign = '-webkit-center';
+	infoLabel.style.paddingTop = '10%';
+	
+	// Building Destination Dropdown Menu
+	var dropdown = document.createElement('div');
+	dropdown.className = "dropdown";
+	
+	var dropbutton = document.createElement('button');
+	dropbutton.className = "dropbutton";
+	dropbutton.innerHTML = "Dropdown";
+	
+	var dropdownContent = document.createElement('div');
+	dropdownContent.className = "dropdown-content";
+	
+	// Build options here
+	for(var i = 0; i < 5; i++) {
+		var option = document.createElement('label');
+		option.innerHTML = "Destination " + (i+1);
+		dropdownContent.appendChild(option);
+	}
+	
+	dropdown.appendChild(dropbutton);
+	dropdown.appendChild(dropdownContent);
+	
+	drunk_menu_container.appendChild(infoLabel);
+	drunk_menu_container.appendChild(dropdown);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
